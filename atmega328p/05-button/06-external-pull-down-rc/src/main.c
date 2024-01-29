da92aff8ace677ec6cd2ab35d3bc299667bb80d0
Maksym Palii <maksympalii.dev@gmail.com>
2024-01-30 00:02:27 +0200

    m328p-05: button, external pull-down, RC debounce

---
+++ atmega328p/05-button/06-external-pull-down-rc/src/main.c
@@ -0,0 +1,38 @@
+/**
+ * @author Maksym Palii
+ * @brief Button with external PULL-DOWN resistor R = 20kΩ; R = 20kΩ and C = 1µF is used for debouncing  
+ * @version 1.0
+ * @date 2024 January 29
+ */
+
+#include <avr/io.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    // Configure data direction for pin 0, 1 (port B) as OUTPUT
+    DDRB |= (_BV(DDB0) | _BV(DDB1));
+    // Keep pin 0 (port C) in Z state (high-impedance)
+    PORTC &= _BV(PORTC0);
+
+    bool button_was_pressed = false;
+
+    while (true)
+    {
+        // check whether button was pressed
+        if (bit_is_set(PINC, PINC0) && !button_was_pressed)
+        {
+            button_was_pressed = true;
+            PORTB ^= _BV(PORTB0);
+            // some event start
+        }
+        
+        // check whether button was released
+        if (bit_is_clear(PINC, PINC0) && button_was_pressed)
+        {
+            button_was_pressed = false;
+            PORTB ^= _BV(PORTB1);
+            // some event finish
+        }
+    }
+}
