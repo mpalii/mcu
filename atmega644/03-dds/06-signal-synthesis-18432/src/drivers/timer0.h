55ee2d1ba1b583dc8b2afab04d53b1d0924ecdee
Maksym Palii <maksympalii.dev@gmail.com>
2024-09-10 20:31:33 +0300

    m644-03: DDS - signal synthesis 18.432MHz

---
+++ atmega644/03-dds/06-signal-synthesis-18432/src/drivers/timer0.h
@@ -0,0 +1,27 @@
+#ifndef TIMER0_H_
+#define TIMER0_H_
+
+#include <stdint.h>
+
+/****************************************************************************/
+/* Set up timer 0 for Fast PWM mode                                         */
+/* 18_432_000Hz / 256 = 72_000Hz;                                           */
+/****************************************************************************/
+inline void init_timer0(void)
+{
+    // TCCR0A – Timer/Counter0 Control Register channel A
+    TCCR0A |= _BV(WGM01) | _BV(WGM00);                  // Waveform Generation Mode - Fast PWM
+    
+    // TCCR0A – Timer/Counter0 Control Register channel A
+    TCCR0A |= _BV(COM0A1);                              // Clear OC0A on Compare Match, set OC0A at BOTTOM, (non-inverting mode)
+	
+    // TCCR0B – Timer/Counter0 Control Register channel B
+    TCCR0B |= _BV(CS00);                                // Start Timer 0 (with clk/1 no prescaling)
+}
+
+inline void set_duty(uint8_t duty)
+{
+    OCR0A = duty;
+}
+
+#endif /* TIMER0_H_ */
