62aa613ec2a9c8cf2851f1547797a41712467fc3
Maksym Palii <maksympalii.dev@gmail.com>
2024-10-13 12:37:21 +0300

    rtm-01: GPIO driver

--- ece4760-spring2012/lw1/reaction-time-measurement/src/main.c
+++ ece4760-spring2012/lw1/reaction-time-measurement/src/main.c
@@ -8,18 +8,42 @@
 
 #define F_CPU (16000000UL)
  
-#include <avr/io.h>
-#include <stdbool.h>
+#include "drivers/gpio.h"
 #include <util/delay.h>
 
-int main(void) 
+const uint8_t pins[] =
 {
-    DDRA |= _BV(PA0);
+    // Port A
+    _GPIO_UNUSED_01, _GPIO_UNUSED_02, _GPIO_UNUSED_03, _GPIO_UNUSED_04,
+    _GPIO_UNUSED_05, _GPIO_UNUSED_06, _GPIO_UNUSED_07, _GPIO_UNUSED_08,
+    // Port B
+    _GPIO_UNUSED_09, _GPIO_UNUSED_10, _GPIO_UNUSED_11, _GPIO_UNUSED_12,
+    _GPIO_UNUSED_13, _GPIO_UNUSED_14, _GPIO_UNUSED_15, _GPIO_UNUSED_16,
+    // Port C
+    _GPIO_UNUSED_17, _GPIO_UNUSED_18, _GPIO_UNUSED_19, _GPIO_UNUSED_20,
+    _GPIO_UNUSED_21, _GPIO_UNUSED_22, _GPIO_UNUSED_23, _GPIO_UNUSED_24,
+    // Port D
+    _GPIO_UNUSED_25, _GPIO_UNUSED_26, _GPIO_UNUSED_27, _GPIO_UNUSED_28,
+    _GPIO_UNUSED_29, _GPIO_UNUSED_30, _GPIO_UNUSED_31, _GPIO_UNUSED_32
+};
+
+inline void toggle_all_pins(void)
+{
+    for (uint8_t i = 0; i < sizeof(pins) / sizeof(pins[0]); i++)
+    {
+        gpio_toggle(pins[i]);
+    }
+}
+
+int main(void)
+{
+    init_gpio();
 
     while (true)
     {
-        PORTA ^= _BV(PA0);
-        _delay_ms(500);
+        toggle_all_pins();
+        _delay_ms(125);
     }
-}   
-  
+
+    return 0;
+}
